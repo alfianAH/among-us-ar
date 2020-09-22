7cb119fd75be2f78e9c19be8c9767abf00dfa82b
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-22 12:17:35 +0800

    Make spawner image

---
+++ Assets/Scripts/ImageHolder.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ImageHolder : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    
+    private void Update()
+    {
+        transform.Translate(0f, speed * Time.deltaTime, 0f);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Sprite[] crewMates, impostors;
+    [SerializeField] private GameObject imageHolder;
+    [SerializeField] private bool isStarting;
+    
+    private BoxCollider2D spawnerCollider;
+
+    private void Start()
+    {
+        spawnerCollider = GetComponent<BoxCollider2D>();
+        
+        // Set size of spawner collider
+        spawnerCollider.size = new Vector2(Screen.width, 100);
+    }
+
+    private void Update()
+    {
+        if (isStarting)
+        {
+            isStarting = false;
+            SpawnPictures();
+        }
+    }
+
+    private void SpawnPictures()
+    {
+        // Duplicate image holder
+        GameObject imageHolderDuplicate = Instantiate(imageHolder, transform.position, Quaternion.identity, transform.parent);
+        // Get image component
+        Image imageHolderSprite = imageHolderDuplicate.GetComponent<Image>();
+        
+        imageHolderSprite.sprite = crewMates[0]; // Set sprite
+    }
+}
